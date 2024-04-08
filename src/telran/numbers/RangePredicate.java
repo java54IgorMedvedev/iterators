3de4f2f48b0e2e3ad3ff6335379de42c81e27859
IgorMedvedev <psydov81@gmail.com>
2024-04-08 18:15:12 +0300

    iterator cw

--- src/telran/numbers/RangePredicate.java
+++ src/telran/numbers/RangePredicate.java
@@ -4,44 +4,56 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
-public class RangePredicate extends Range {
-    private Predicate<Integer> predicate = n -> true; // Инициализируем предикат значением по умолчанию, которое принимает все числа.
+public class RangePredicate extends Range{
+	private Predicate<Integer> predicate;
+	protected RangePredicate(int min, int max) {
+		super(min, max);
+		
+	}
+	public void setPredicate(Predicate<Integer> predicate) {
+		this.predicate = predicate;
+	}
+	public static RangePredicate getRange(int min, int max) {
+		checkMinMax(min, max);
+		return new RangePredicate(min, max);
+	}
+	@Override
+	public Iterator<Integer> iterator() {
+		return new RangePredicateIterator();
+	}
+	private class RangePredicateIterator implements Iterator<Integer> {
+		long current = min - 1;
+		RangePredicateIterator(){
+			if(predicate == null) {
+				predicate = x -> true;
+			}
+			setCurrent();
+		}
+		@Override
+		public boolean hasNext() {
+			
+			return current <= max;
+		}
 
-    protected RangePredicate(int min, int max) {
-        super(min, max);
-    }
+		@Override
+		public Integer next() {
+			if(!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			int result = (int)current;
+			setCurrent();
+			return result;
+		}
 
-    public void setPredicate(Predicate<Integer> predicate) {
-        this.predicate = predicate; // Исправляем здесь на правильное присваивание
-    }
+		private void setCurrent() {
+			current++;
+			while(current <= max && !predicate.test((int) current)) {
+				current++;
+			}
+			
+		}
+		
+		
+	}
 
-    public static RangePredicate getRange(int min, int max) {
-        checkMinMax(min, max);
-        return new RangePredicate(min, max);
-    }
-
-    @Override
-    public Iterator<Integer> iterator() {
-        return new RangePredicateIterator();
-    }
-
-    private class RangePredicateIterator implements Iterator<Integer> {
-        private int current = min;
-
-        @Override
-        public boolean hasNext() {
-            while (current <= max && !predicate.test(current)) {
-                current++;
-            }
-            return current <= max;
-        }
-
-        @Override
-        public Integer next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return current++;
-        }
-    }
 }
